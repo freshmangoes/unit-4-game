62c41ff50fff85eac58fb76f58919114a3a49d94
Kyle C <kylecilia@gmail.com>
2019-10-17 19:46:46 -0700

    Adds some comments for clarity. [WIP]

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,20 @@
 var gameState = {
+  // jQuery selectors for gems
   gems: {
-    gem1 : $(".gem1"),
-    gem2 : $(".gem2"),
-    gem3 : $(".gem3"),
+    gem1: $(".gem1"),
+    gem2: $(".gem2"),
+    gem3: $(".gem3"),
+    gem4: $(".gem4")
   },
 
+  // jQuery selectors for game display
   display: {
     score: $(".score"),
     wins: $(".wins"),
     losses: $(".losses")
   },
 
+  // Game values, goal and game values
   values: {
     goal: 0,
     gem1: 1,
@@ -18,7 +22,8 @@ var gameState = {
     gem3: 1,
     gem4: 1
   },
-  
+
+  // Gives each values.gem# a value by using randomGemVal()
   setGemVals() {
     this.values.gem1 = this.randomGemVal();
     this.values.gem2 = this.randomGemVal();
@@ -26,22 +31,25 @@ var gameState = {
     this.values.gem4 = this.randomGemVal();
   },
 
-  randomGoal() {
-    this.values.goal = Math.floor(Math.random() * (120-19) + 19);
+  // Sets goal
+  setGoal() {
+    this.values.goal = Math.floor(Math.random() * (120 - 19) + 19);
   },
 
+  // Creates a random value between 1 and 12
   randomGemVal() {
-    var gameVal = Math.floor(Math.random() * (12-1) + 1);
+    var gameVal = Math.floor(Math.random() * (12 - 1) + 1);
     console.log('gameVal:', gameVal);
     return gameVal;
   },
 
-
+  // Initializes game
   init() {
-    this.randomGoal();
+    this.setGoal();
     this.setGemVals();
   },
 
+  // Updates game, WIP
   update() {
 
   },
